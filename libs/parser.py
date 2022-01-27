e0bd57a597a4dd99481c361c2b1827036420aa56
tarpalantiri <tehseensajjadsb@gmail.com>
2022-01-27 23:18:13 +0500

    [Add] make_gate func that makes Gate objects

--- libs/parser.py
+++ libs/parser.py
@@ -1,5 +1,5 @@
 import yaml
-from .models import Pod, Pod_Coordinates
+from .models import Pod, Pod_Coordinates, Gate_Info, Gate
 
 KNOWN_DOCUMENTS = ['Pods', 'Gates', 'Airlocks', 'Astronauts']
 
@@ -14,16 +14,16 @@ def parse_yaml(yaml_file_path="./facility.yaml"):
     return [*all_docs]
 
 def make_pod(index, parsed_dict):
-    sanitized = dict()
+    formated_dict = dict()
     # Turn all key strings to lower case, so the dataclass can be
     # given a dict as a parameter
-    parsed_dict = {key.lower() : value for key, value in parsed_dict}
+    parsed_dict = {key.lower() : value for key, value in parsed_dict.items()}
     
     # Adding Pod Index
-    sanitized["pod_index"] = int(index)
+    formated_dict["pod_index"] = int(index)
     
     # Constructing Coordinates
-    sanitized['coords'] = Pod_Coordinates(
+    formated_dict['coords'] = Pod_Coordinates(
         north=parsed_dict["North"],
         east=parsed_dict["East"],
         south=parsed_dict["South"],
@@ -36,9 +36,20 @@ def make_pod(index, parsed_dict):
             # Turn all the 1s and 0s to bools
             if isinstance(value, int):
                 value = bool(value)
-            sanitized[key] = value
-    return Pod(**sanitized)
+            formated_dict[key] = value
+    return Pod(**formated_dict)
 
-    
-    
+def make_gate(gate_id, parsed_dict):
+    formated_dict = dict()
+    parsed_dict = { key.lower(): value for key, value in parsed_dict.items() }
+    formated_dict['gate_id'] = gate_id
+    # Add gate info
+    for gate_loc, gate_info in parsed_dict.items():
+        # Changing all parameters to lower_case for the inner dict
+        gate_info = { key.lower():value for key, value in gate_info.items() }
+        
+        # Making a Gate_Info obejct
+        formated_dict[gate_loc] = Gate_Info(**gate_info)
+    return Gate(**formated_dict)
+        
     
